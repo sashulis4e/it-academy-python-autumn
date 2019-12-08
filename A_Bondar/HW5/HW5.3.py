d3893e5f0e8fbbf01842acfe75b65ee8383895d7
sashulis4e <y_balloon@list.ru>
2019-12-08 19:22:45 +0300

    Homework2_A.Bondar

---
+++ A_Bondar/HW5/HW5.3.py
@@ -0,0 +1,14 @@
+#  Дан список стран и городов каждой страны. Затем даны названия городов.
+#  Для каждого города укажите, в какой стране он находится.
+
+countries = {}
+for i in range(int(input('Количество стран'))):
+    country, *city = input('Страны и их города').split()
+    countries[country] = city
+
+cities = []
+for k in range(int(input('Количество городов'))):
+    city = input('Город')
+    cities.append(city)
+    for city in cities:
+        print(countries.get(city))
